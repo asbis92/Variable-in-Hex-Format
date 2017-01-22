c80d9be6d268b1410b14f2e42192d442a08c16c8
asbis92 <rdsl_ivanov@yahoo.com>
2017-01-22 16:20:41 +0200

    Visual improvement

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace _04.Variable_in_Hex_Format
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number in hexadecimal format: ");
             string hexaDecimalValue = Console.ReadLine();
             int decimalValue = Convert.ToInt32(hexaDecimalValue, 16);
 
