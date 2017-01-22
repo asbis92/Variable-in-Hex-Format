05cd40bb53e58f7833af05cf569b0c50f0625790
asbis92 <rdsl_ivanov@yahoo.com>
2017-01-22 16:15:42 +0200

    Converter

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Variable_in_Hex_Format
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hexaDecimalValue = Console.ReadLine();
+            int decimalValue = Convert.ToInt32(hexaDecimalValue, 16);
+
+            Console.WriteLine(decimalValue);
+
+
+        }
+    }
+}
